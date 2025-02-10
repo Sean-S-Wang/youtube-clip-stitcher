24994fc1a8934f7814acc5293fa628222ba23c93
Sean Wang <seanw@terpmail.edu>
2025-02-10 00:17:03 -0500

    Changed to use text file instead of example usage

--- main.py
+++ main.py
@@ -107,36 +107,9 @@ def process_clips(clip_urls, output_folder="downloads", final_video_name="final_
 
 
 # Example usage
-clip_urls = [
-    "https://www.youtube.com/clip/Ugkxyl80X_un31ttZ-mcMstosh9jMiarqHoY",
-    "https://www.youtube.com/clip/Ugkx04MBG-dtuh0K4NG4U9gnjKxLQOB2uFOO",
-    "https://youtube.com/clip/UgkxPID4-UyGpIYIdN2YwpDuWZUXyNTU2R_t?si=yA_IR7x9l0UObD9n",
-    "https://youtube.com/clip/UgkxBxK-o56yBPdnqrmf5gamiseoatkynVYP?si=F9o3goJfFxAPfjqR",
-    "https://youtube.com/clip/Ugkx32lDO7WDATZj0edecJPIij-W--zuVig7?si=W_zsnMruMfV5j4Ox",
-    "https://youtube.com/clip/Ugkx29-T7DwcB0GQhX3fhfN3_BBJ_YQuSV4p?si=pYDQHX36WtbtUjfo",
-    "https://youtube.com/clip/UgkxJm4jGmS35FSCeSnyfVkuShgv56ucEdof?si=1KVXQJf44M38CM9D",
-    "https://youtube.com/clip/UgkxAZADrTne5weHcbj8tLDz1hMrraUlSyu3?si=FEl5mxpBlXhTE2oh",
-    "https://youtube.com/clip/Ugkxr-9-pmPefXO3O_xgX4OKgFSMADvSsQab?si=p9zqDeH5iTkR3HzB",
-    "https://youtube.com/clip/Ugkx9K9rYaVN5Ml0xolH5vXjn5NFBqnUb_SY?si=tCsexAkGjlYP_I-v",
-    "https://youtube.com/clip/Ugkx0v07MljlpYuameXbKj_a48ItfyjtJTMe?si=X3xMt_-HdItfW_dx",
-    "https://youtube.com/clip/UgkxL26XePGkj9J2avjJ76OKeUdN30pKqgAW?si=oMYFybjn0lDrFpSk",
-    "https://youtube.com/clip/Ugkx15FumHdkmmV0A1HcryF0AbUqcc5BPK0k?si=Qs8y6cGc-czIyBXF",
-    "https://youtube.com/clip/UgkxX9bzIZu8-NHyDlQMWi5Y4yAgeCGYPBLF?si=1wm81VizltMutK6x",
-    "https://youtube.com/clip/Ugkx5qmNNs4ICLLm-lMCnOr0aJNslTUGBRsG?si=Mm_46FovIeVOGy9T",
-    "https://youtube.com/clip/Ugkxs41aPQRs5lopY-5bYjcO1yBmmBPEWmkz?si=q6AGE4xomjnzMqne",
-    "https://youtube.com/clip/UgkxSzdYCaZDhpxfWfZ5kO2iR69oKq5SpHl1?si=riVzr_M528jAN0N_",
-    "https://youtube.com/clip/UgkxXTNgBCb_ip-ye_HBkYCQhT9NHQOGV5BF?si=ZSYvJr5n3U1pgOGc",
-    "https://youtube.com/clip/Ugkx34pQXCtYkl3qKr2-vJk8ekoUctoyfvsw?si=QeAwDLqJt8IS0rY0",
-    "https://youtube.com/clip/UgkxLGAZZH-rJ6EhhFZaMUXAAXnWzber2xiX?si=GGWByzJp0eETV07G",
-    "https://youtube.com/clip/UgkxzimX7TEBCRGD3gCErkjgpcSYPuiypZUz?si=j6wcytnIz0mpCNK5",
-    "https://youtube.com/clip/Ugkxs1_QXmRY_iLzWH9Cflx8Glgze32JrQ4r?si=xtDsNFwVFxRTpjC7",
-    "https://youtube.com/clip/Ugkxzm1CcKGqRZVg1snjavu6dV4eQuXaSGn1?si=h3b9I_2gHsdBaVoU",
-    "https://youtube.com/clip/UgkxSthsWUZid2WpzuXgfX5_eONFb57JzxoI?si=erU7kU5yIbobGStj",
-    "https://youtube.com/clip/Ugkxe9Xoc1qfe0Zcj52wyYp3zRX2_qWnC3AB?si=DScQCHfagy0Bgx1f",
-    "https://youtube.com/clip/UgkxlVeVqXEOY0A4kIDPbIbMU-AMXWjoU9TK?si=o6-p9x_iLPMaQ9-4",
-    "https://youtube.com/clip/UgkxYbXdyO5wpbgcTNH4GgFI9UM794hXPdsu?si=WHNebA0P52CQp9bY",
-    "https://youtube.com/clip/UgkxV8yAOFT2F-X6dCYwg_6xm_6sGXgXPXYo?si=NGO6wnGycyb9WEXA",
-    "https://youtube.com/clip/UgkxGRikU_51p6meNC3PuDs4PS6TmoNktYtP?si=gPc2lR992Bd6ziZb",
-]
+file_path = "clip_urls.txt"
+with open(file_path, 'r') as file:
+    lines = [line.strip() for line in file]
+    clip_urls = lines
 
 process_clips(clip_urls)
